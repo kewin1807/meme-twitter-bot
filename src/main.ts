f4531b00f7e183a8ff8661f7ffccee1153408c1b
cuongnh1807 <hungcuong@remitano.com>
2025-01-23 12:00:15 +0700

    Update schduler

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,68 @@
+import twitterService from "./services/twitter.service";
+import prisma from './services/prisma.service';
+import { extractTweetFromGrok, formatResult, formatTelegramMessage } from "./utils";
+import telegramCommands from "./bot.handler";
+import schedule from 'node-schedule';
 import express, { Request, Response } from 'express';
-import { authRouter } from "./routers";
-import TelegramBot from 'node-telegram-bot-api';
-import { telegramBot } from "./utils"
-import telegramCommands from './bot.handler';
-
 
 const app = express();
 const PORT = 3000;
 
-
-
-// Middleware
 app.use(express.json());
-
-// Routes
-// auth
-app.use("/auth", authRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
 
-
-
-
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+async function scheduler() {
+  const telegramBot = telegramCommands.getInstance();
+  const kols = await prisma.kol.findMany();
+  const extractedTweets = []
+  for (const kol of kols) {
+    console.log(`Getting latest tweet for ${kol.handleName}`);
+    const tweets = await twitterService.getLatestTweet(kol.handleName);
+    if (tweets && (tweets.id !== kol.lastPostId || kol.lastPostId === null)) {
+      // update lastPostId
+      await prisma.kol.update({
+        where: { id: kol.id },
+        data: { lastPostId: tweets.id }
+      });
+      extractedTweets.push({ kol_name: kol.handleName, tweet: tweets });
+    }
+  }
+
+  for (const tweet of extractedTweets) {
+    console.log(`Extracting tweet id: ${tweet.tweet.id}`);
+    const result = await extractTweetFromGrok(tweet.tweet);
+    const formattedResult = await formatResult(result);
+    if (result.token || result.contract) {
+      if (formattedResult) {
+        formattedResult.mentioned_by = tweet.kol_name;
+        formattedResult.post_link_url = `https://x.com/${tweet.kol_name}/status/${tweet.tweet.id}`;
+        formattedResult.summary = result.summary || tweet.tweet.text;
+        try {
+          const telegramMessage = formatTelegramMessage(formattedResult);
+          await telegramBot.sendMessage(process.env.TELEGRAM_CHANNEL_ID || '', telegramMessage, {
+            parse_mode: 'MarkdownV2',
+            disable_web_page_preview: true
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  }
+}
+
+schedule.scheduleJob('*/5 * * * *', async () => {
+  console.log('Running scheduler job at:', new Date().toISOString());
+  await scheduler();
 });
 
 console.log('Telegram bot initialized');
@@ -34,3 +70,6 @@ console.log('Telegram bot initialized');
 // Initialize bot commands()
 telegramCommands.initializeCommands();
 console.log('Bot commands registered');
+
+scheduler();
+
