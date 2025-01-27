52f679070e0a95a83ac8d8c78d0c60b155600c3f
cuongnh1807 <hungcuong@remitano.com>
2025-01-27 22:36:30 +0700

    Remove test

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,6 @@ import { extractTweetFromGrok, formatResult, formatTelegramMessage } from "./uti
 import twitterService from "./services/twitter.service";
 (async () => {
   const tweets = await twitterService.getTweetById('1882980508596142267');
-  console.log(tweets);
   if (tweets) {
     const result = await extractTweetFromGrok(tweets);
     console.log(result);
