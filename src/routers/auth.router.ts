9132bcfb8304cc01270de6b590b76cd49fcd1f95
cuongnh1807 <hungcuong@remitano.com>
2025-01-27 14:53:14 +0700

    Remove auth service

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -5,6 +5,4 @@ const authController = new AuthController()
 
 const authRouter = Router();
 
-authRouter.post('/kol', authController.createKol);
-authRouter.get('/kols', authController.getAllKols);
 export { authRouter }
